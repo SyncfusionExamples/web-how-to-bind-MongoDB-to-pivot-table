30601a31735d190bfbf8179feb6df6065b21e665
AngelinFaithSheeba <angelinfaithsheeba.p@syncfusion.com>
2024-10-28 21:00:19 +0530

    917032: Fixed dependabot issue.

--- VUE/pivot-table/src/main.js
+++ VUE/pivot-table/src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
+import './App.css'
 import App from './App.vue'
+
+const NotFound = { template: '<p>Page not found</p>' }
+
+const routes = {
+  '/': App,
+}
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
 })
